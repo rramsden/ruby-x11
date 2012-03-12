52c38bd394b865044d774492991f19cab1836e6e
Matthias Beyer <matthias@ib-fb.de>
2012-03-12 18:45:47 +0100

    add Keycode type and CustomType for abstract Types
    Was not able to split the commit into two
    
    Keycode type is simply a Uint8
    (and Button type, too! But Button type is outcommented because still unused)
    
    add also a CustomType class for defining complicated/unusual types
    (E.G.: The Screen package gets a field named "backing-stores" which can
    be
    0 for Never
    1 for WhenMapped
    2 for Always
    this can be represented like this:
    
    CustomType.new() do |never, when_mapped, always|
    never = 0
    when_mapped = 1
    always = 2
    end
    
    The instance variables will be set automatically. The object just needs
    to be interpreted the right way.
    
    I'm not sure if I understood the whole thing, but if, this helps to
    define types which can have just x values (as in the example).

--- lib/X11/encode.rb
+++ lib/X11/encode.rb
@@ -27,5 +27,52 @@ module X11
     Uint16    = pack("S")
     Uint32    = pack("L")
     String8   = lambda {|a| pad(a)}
+
+    Keycode   = Uint8
+    #Button    = Uint8
+    
+    # a CustomType class
+    # instance variables can be set by the block
+    # E.G.:
+    # CustomType.new() { |a_var, an_other_var| a_var = 5; an_other_var = 10 }
+    # will create a CustomType object with 
+    #  @a_var = 5
+    #  @an_other_var = 10
+    #
+    # 
+    class CustomType
+      def initialize &block
+        ivars = block.parameters.map do |ary|
+          name = ary.last
+          name = name.to_s.insert(0, "@" ).to_sym unless name.to_s.start_with? "@"
+          name
+        end
+        ivars.each { |vname| instance_variable_set vname, nil }
+        ivars.map! { |iv| instance_variable_get iv } # instance variable name to instance variable
+        yield *ivars
+      end
+    end
+
+    VisualID # is an Atom ? 
+    Bool # { true, false }
+    Event #{KeyPress, KeyRelease, OwnerGrabButton, ButtonPress,
+    # ButtonRelease, EnterWindow, LeaveWindow, PointerMotion,
+    # PointerMotionHint, Button1Motion, Button2Motion,
+    # Button3Motion, Button4Motion, Button5Motion, ButtonMotion,
+    # Exposure, VisibilityChange, StructureNotify, ResizeRedirect,
+    # SubstructureNotify, SubstructureRedirect, FocusChange,
+    # PropertyChange, ColormapChange, KeymapState}
+
+    Window    # 32-bit value (top three bits guaranteed to be zero)
+    ColorMap  # 32-bit value (top three bits guaranteed to be zero)
+
+    def list klass
+      # do stuff
+    end
+
+    def set klass
+      # do stuff
+    end
+    
   end
 end
