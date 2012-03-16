3ecaac170aa1b7d0768c4353913cd88d5fabd130
unknown <z00305sy@md10t1vc.ww001.siemens.net>
2012-03-16 11:49:33 +0100

    added comments: information from pdf for connection accespted

--- lib/X11/packet.rb
+++ lib/X11/packet.rb
@@ -46,6 +46,38 @@ module X11
       field :auth_proto_data, String8
     end
 
+    #
+    # Information received by the client if the connection is accepted:
+    #
+    #   1   1               Success
+    #   1                   unused
+    #   2   CARD16          protocol-major-version
+    #   2   CARD16          protocol-minor-version
+    #   2   8+2n+(v+p+m)/4  length in 4-byte units of ‘‘additional data’’
+    #   4   CARD32          release-number
+    #   4   CARD32          resource-id-base
+    #   4   CARD32          resource-id-mask
+    #   4   CARD32          motion-buffer-size
+    #   2   v               length of vendor
+    #   2   CARD16          maximum-request-length
+    #   1   CARD8           numberof SCREENs in roots
+    #   1   n               number for FORMATs in pixmap-formats
+    #   1                   image-byte-order
+    #   0                   LSBFirst
+    #   1                   MSBFirst
+    #   1                   bitmap-format-bit-order
+    #   0                   LeastSignificant
+    #   1                   MostSignificant
+    #   1   CARD8           bitmap-format-scanline-unit
+    #   1   CARD8           bitmap-format-scanline-pad
+    #   1   KEYCODE         min-keycode
+    #   1   KEYCODE         max-keycode
+    #   4                   unused
+    #   v   STRING8         vendor
+    #   p                   unused, p=pad(v)
+    #   8n  LISTofFORMAT    pixmap-formats
+    #   m   LISTofSCREEN    roots(m is always a multiple of 4)
+    #
     class ConnectionAccept < BasePacket
       field :success #, type?
       field :static, Unused
@@ -80,6 +112,14 @@ module X11
 
     class SubPackage < BasePackage; end
 
+    #
+    #   FORMAT
+    #   
+    #   1   CARD8     depth
+    #   1   CARD8     bits-per-pixel
+    #   1   CARD8     scanline-pad
+    #   5             unused
+    #
     class Format < SubPackage
       field :depth, Uint8
       field :bits_per_pixel, Uint8
@@ -87,6 +127,30 @@ module X11
       field :static, Unused
     end
 
+    #   
+    #   SCREEN
+    #
+    #   4   WINDOW              root
+    #   4   COLORMAP            default-colormap
+    #   4   CARD32              white-pixel
+    #   4   CARD32              black-pixel
+    #   4   SETofEVENT          current-input-masks
+    #   2   CARD16              width-in-pixels
+    #   2   CARD16              height-in-pixels
+    #   2   CARD16              width-in-millimeters
+    #   2   CARD16              height-in-millimeters
+    #   2   CARD16              min-installed-maps
+    #   2   CARD16              max-installed-maps
+    #   4   VISUALID            root-visual
+    #   1                       backing-stores
+    #   0                         Never
+    #   1                         WhenMapped
+    #   2                         Always
+    #   1   BOOL                save-unders
+    #   1   CARD8               root-depth
+    #   1   CARD8               numberof DEPTHs in allowed-depths
+    #   n   LISTofDEPTH         allowed-depths (n is always a multiple of 4)
+    #
     class Screen < SubPackage
       field :root, Window
       field :default_colormap, ColorMap
@@ -110,6 +174,16 @@ module X11
       field :allowed_depths, list( Depth )
     end
 
+    #
+    #   DEPTH
+    #   
+    #   1   CARD8                 depth
+    #   1                         unused
+    #   2   n                     number of VISUALTYPES in visuals
+    #   4                         unused
+    #   24n   LISTofVISUALTYPE    visuals
+    #   119
+    #
     class Depth < SubPackage
       field :depth, Uint8
       field :static, Unused
@@ -118,6 +192,24 @@ module X11
       field :visuals, list( VisualType )
     end
 
+    #
+    #   VISUALTYPE
+    #
+    #   4   VISUALID              visual-id
+    #   1                         class
+    #   0                           StaticGray
+    #   1                           GrayScale
+    #   2                           StaticColor
+    #   3                           PseudoColor
+    #   4                           TrueColor
+    #   5                           DirectColor
+    #   1   CARD8                 bits-per-rgb-value
+    #   2   CARD16                colormap-entries
+    #   4   CARD32                  red-mask
+    #   4   CARD32                  green-mask
+    #   4   CARD32                  blue-mask
+    #   4                           unused
+    #
     class VisualType < SubPackage
       field :visual_id, VisualID
       #field :class, type?: CustomType.new() { |static_gray, ... | ... }
