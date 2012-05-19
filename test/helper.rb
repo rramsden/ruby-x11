93a15e6afaac753a0e35c3d8122d5c650ec02357
Richard Ramsden <richard@rramsden.ca>
2012-05-19 11:49:44 -0700

    start adding tests

--- test/helper.rb
+++ test/helper.rb
@@ -1,18 +1,11 @@
 require 'rubygems'
 require 'bundler'
-begin
-  Bundler.setup(:default, :development)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
-require 'test/unit'
-require 'shoulda'
+Bundler.setup(:default, :development)
+
+require 'minitest/spec'
+require 'minitest/autorun'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'X11'
 
-class Test::Unit::TestCase
-end
+require 'X11'
